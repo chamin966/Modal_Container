8504ebb2798b2529661bfc51b817a5c23d92dd57
armin <armin@irm.kr>
2023-12-17 17:26:46 +0900

    [feat] Link 태그로 홈화면간 라우팅 기능 추가

--- my-app/app/apps/page.tsx
+++ my-app/app/apps/page.tsx
@@ -1,7 +1,13 @@
+'use client';
+import BasicModal from '@/components/BasicModal';
+import Link from 'next/link';
+
 export default function Apps() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className='flex min-h-screen flex-col items-center justify-center p-24'>
       <div>Apps 입니다</div>
+      <BasicModal />
+      <Link href={{ pathname: '/' }}>홈화면으로 돌아가기</Link>
     </main>
   );
 }
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,11 +1,10 @@
-'use client';
-import BasicModal from '@/components/BasicModal';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className='flex  min-h-screen flex-col items-center justify-center p-24 ga'>
       <div>홈화면 입니다</div>
-      <BasicModal />
+      <Link href={{ pathname: '/apps' }}>apps 화면으로 이동</Link>
     </main>
   );
 }
